ea5007c1160d5a59ffd1f546d4bb1f4d5a98ee11
trliner <trliner@gmail.com>
2011-09-01 21:18:57 -0500

    start off with an attribute reader

--- lib/fake_id.rb
+++ lib/fake_id.rb
@@ -1,3 +1,31 @@
 module FakeId
 
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+
+    def fake_ids_for(column_name, id_mapping)
+      column_id = (column_name.to_s + "_id").to_sym
+
+      # mappings are stored in a class level hash
+      class_inheritable_hash :fake_id_mappings
+      write_inheritable_attribute(:fake_id_mappings, {}) if fake_id_mappings.nil?
+      fake_id_mappings[column_name] ||= {}
+
+      id_mapping.each do |name, id|
+        fake_id_mappings[column_name][id] = name
+      end
+
+      class_eval <<-EVAL
+        def #{column_name}
+          self.class.fake_id_mappings[:#{column_name}][self.#{column_id}]
+        end
+      EVAL
+
+    end
+
+  end
+
 end
